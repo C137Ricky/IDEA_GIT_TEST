b418abdf1b6b26f75f2cee15339c21b7b01c48bc
QUZHIHAO <qzh921314@qq,com>
2020-09-15 10:11:23 +0800

    第e二次修改

--- Facterory/BMW320.java
+++ Facterory/BMW320.java
@@ -4,5 +4,11 @@ package Facterory;
 public class BMW320 extends BMW {
     public BMW320() {
         System.out.println("制造-->BMW320");
+
+    }
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
+
+
