8e4f804d8b571a27e4617729431033c530c8a943
QUZHIHAO <qzh921314@qq,com>
2020-09-15 10:24:31 +0800

    修改传到GitHub

--- Singleton/Singleton1.java
+++ Singleton/Singleton1.java
@@ -3,7 +3,8 @@ package Singleton;
 public class Singleton1 {
    private  Singleton1(){};
    private static final Singleton1 single=new Singleton1();
-
+   //1223455
+    //445
    private  static Singleton1 getInstance(){
            return single;
 }
